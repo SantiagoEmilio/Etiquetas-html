16626fdafd22f54bcbf0ec5197680374cc389224
Iskrisss <klfpaz@scl.edu.gt>
2024-05-30 20:43:38 -0600

    Seccion 2 terminada Kristhel

--- secction2/secction2.js
+++ secction2/secction2.js
@@ -1,3 +1,52 @@
 let seccion2_DoM = document.querySelector(".seccion2");
 
-seccion2_DoM.innerHTML = `Hola soy seccion2`;
+seccion2_DoM.innerHTML = `<div class="table-container">
+<h1>Principales Etiquetas HTML</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Etiqueta</th>
+          <th>Descripción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>header</td>
+          <td>Representa un contenedor para contenido introductorio o navegacional</td>
+        </tr>
+        <tr>
+          <td>nav</td>
+          <td>Define un conjunto de enlaces de navegación</td>
+        </tr>
+        <tr>
+          <td>footer</td>
+          <td>Define un pie de página para un documento o sección</td>
+        </tr>
+        <tr>
+          <td>section</td>
+          <td>Define una sección en un documento</td>
+        </tr>
+        <tr>
+          <td>article</td>
+          <td>Define contenido independiente y auto-contenido</td>
+        </tr>
+        <tr>
+          <td>div</td>
+          <td>Define una sección o división en un documento</td>
+        </tr>
+        <tr>
+          <td>span</td>
+          <td>Define una sección en línea en un documento</td>
+        </tr>
+        <tr>
+          <td>button</td>
+          <td>Define un botón clicable</td>
+        </tr>
+        <tr>
+          <td>table</td>
+          <td>Define una tabla</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+`;
