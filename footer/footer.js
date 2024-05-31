fb84bbe74c07402073d358937f410ef2d8d972a3
Santiago <slopez@scl.edu.gt>
2024-05-30 18:08:28 -0600

    nueva verción del header y css

--- footer/footer.js
+++ footer/footer.js
@@ -1,4 +1,8 @@
 let footer_DoM = document.querySelector(".footer");
 
 footer_DoM.innerHTML = `
-<p class="titulo_final">hola soy footer santi</p>`;
+<p class="titulo_final">hola soy footer santi</p> 
+<a href="https://github.com/Aandreeemel">Andre colaborador </a>
+<a href="https://github.com/Kristhellfp">Kristhel colaborador</a>
+<a href="https://github.com/MG5Gabri"> Gabriel colaborador </a>
+<img src="https://static.wixstatic.com/media/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png/v1/fit/w_2500,h_1330,al_c/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png" alt="" class="logo">`;
